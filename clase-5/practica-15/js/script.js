56e13c210720669883eea3c7049368b316e1548e
angelescalvo5 <labandadelacorne2019@gmail.com>
2026-05-27 00:18:32 -0300

    clase 7

---
+++ clase-5/practica-15/js/script.js
@@ -0,0 +1,2 @@
+//hola mi primer coment en java
+console.log("hola mundo ñengoflow");
---
+++ clase-5/practica-16/js/archivo.js
@@ -0,0 +1,12 @@
+//variables de nombre y apellido
+let miNombre ="Angeles"
+
+let miApellido="Calvo"
+
+let nombreCompleto={
+ nombre:"miNombre",
+ apellido:"miApellido"
+}
+let miEdad ="18"
+
+const miCufa="UNA Artes Multimediales"
